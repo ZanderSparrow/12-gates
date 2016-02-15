dc935c25df39b87329b44034949d3f22081720fd
Silverhawk <sondra.silverhawk@hush.com>
2016-02-15 15:25:30 -0800

    Add descriptions for each enemy

--- js/game.js
+++ js/game.js
@@ -49,60 +49,72 @@ var app = angular.module('12app', [])
     {
       name: 'Cacophony',
       message: 'You are hit by a phrenetic wave of light and sound...',
+      description: 'You awaken in a world of light, sound, and sensation. Nothing makes sense, and everything is too much.',
       difficulty: 1,
     },
     {
       name: 'The Others',
       message: 'Their gaze pierces you.',
+      description: 'Suddenly you notice them everywhere, staring, but what are they thinking?',
       difficulty: 2,
     },
     {
       name: 'Royal Reflection',
       message: 'You are shaken by what you see but cannot recognize.',
+      description: 'There is a person in the glass who is both familiar and so unfamiliar.',
       difficulty: 3,
     },
     {
       name: 'The Distant Land',
       message: 'You are hypnotized by the beauty.',
+      description: 'When you realize there is another land you cannot look away, but how can you get there?',
       difficulty: 4,
     },
     {
       name: 'Giant Dreams',
       message: 'He crushes you with his foot.',
+      description: 'You are pulled toward his powerful presence.',
       difficulty: 5,
     },
     {
       name: 'The Costume',
       message: 'You forget who you are, and fall...',
+      description: 'If you wore it you would be invisible in one world, and visible in another.',
       difficulty: 6,
     },
     {
       name: 'Prison Head',
       message: 'He shocks you into submission!',
+      description: 'He says that if you break away you cease to be. You hope it\'s a lie...',
       difficulty: 7,
     },
     {
       name: 'Visionary',
       message: 'She stuns you as you try to see her.',
+      description: 'What she shows you is so beautiful, but can it be real?',
       difficulty: 8,
     },
     {
       name: 'Shifter Monster',
       message: 'He phases into a new form and sneaks past your defenses.',
+      description: 'All the visions that were clear are scrambled into chaos!',
       difficulty: 9,
     },
     {
       name: 'A Certain Star',
       message: 'An arrow hits its mark!',
+      description: 'You can\'t stop watching the arrows. You want to see which direction is right.',
       difficulty: 10,
     },
     {
       name: 'Unstoppable Rock',
       message: 'He smacks you down.',
+      description: 'He won\'t move no matter how nicely you ask...',
       difficulty: 11,
     },
     {
       name: 'The Last One',
+      description: '',
       message: '...',
       difficulty: 12,
     }
