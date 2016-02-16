de42890fb6c1c3eb16e87fab572d9b38541c07d5
Silverhawk <sondra.silverhawk@hush.com>
2016-02-16 14:48:36 -0800

    Add nasa pic of the day to epilogue

--- js/mechanics.js
+++ js/mechanics.js
@@ -1,12 +1,27 @@
 angular.module('game.mechanics', [])
 
-.factory('Mech', function () {
+.factory('Mech', ['$http', function ($http) {
   var getHit = function (level) {
     // calculates how many hp an attack will deduct
     return Math.ceil(level * 30 * Math.random()) + level;
   };
 
+  var getSpace = function () {
+    var url = "https://api.nasa.gov/planetary/apod?api_key=ohzc02SgZ2DOYdqhqKs44E5v6T4dOHl5GDlE6B8K";
+    return $http({
+      method: 'GET',
+      url: url,
+    })
+    .then(function (res) {
+      return res;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
   return {
-    getHit: getHit
+    getHit: getHit,
+    getSpace: getSpace
   } 
-});
+}]);
--- js/space.js
+++ js/space.js
@@ -1,4 +1,13 @@
 angular.module('game.space', [])
-  .controller('spaceCtrl', ['$scope', function ($scope) {
-
+  .controller('spaceCtrl', ['$scope', 'Mech', function ($scope, Mech) {
+    $scope.space = {};
+    $scope.spaceKeys = [];
+    Mech.getSpace()
+    .then(function (res) {
+      $scope.space = res.data;
+      $scope.spaceKeys = Object.keys(res.data);
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
   }]);
