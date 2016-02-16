be7fe3d09529a627d87243be60670c1e0bc8518d
Silverhawk <sondra.silverhawk@hush.com>
2016-02-16 10:38:44 -0800

    Hit button reduces player life using getHit from Mech factory

--- js/end.js
+++ js/end.js
@@ -1,5 +1,5 @@
 var app = angular.module('game.end', [])
-  .controller('endCtrl', ['$scope', '$timeout', '$location', function($scope, $timeout, $location) {
+  .controller('endCtrl', ['$scope', 'Mech', '$timeout', '$location', function ($scope, Mech, $timeout, $location) {
     // link to game mech factory
     $scope.playerHealth = 1200;
 
@@ -8,6 +8,7 @@ var app = angular.module('game.end', [])
       $timeout(function () {
         $scope.playerTurn = false;
         $scope.enemyTurn = true;
+        $scope.enemyHit = Mech.getHit(12);
         $scope.playerHealth = $scope.playerHealth - $scope.enemyHit >= 0 ? $scope.playerHealth - $scope.enemyHit : 0;
         // when health gets to 0 load end screen
         if($scope.playerHealth === 0) {
